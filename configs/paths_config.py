b28d73026445308953fb49f9e85568c927c53b08
Gabriele Sarti <gabriele.sarti996@gmail.com>
2021-07-26 10:23:53 +0200

    Update paths_config.py

--- configs/paths_config.py
+++ configs/paths_config.py
@@ -14,12 +14,12 @@ dataset_paths = {
 
 model_paths = {
 	'stylegan_fashion': '/app/pretrained_models/stylegan2-fashion-config-f.pt',
-	'stylegan_ffhq': 'pretrained_models/stylegan2-ffhq-config-f.pt',
-	'ir_se50': 'pretrained_models/model_ir_se50.pth',
-	'circular_face': 'pretrained_models/CurricularFace_Backbone.pth',
-	'mtcnn_pnet': 'pretrained_models/mtcnn/pnet.npy',
-	'mtcnn_rnet': 'pretrained_models/mtcnn/rnet.npy',
-	'mtcnn_onet': 'pretrained_models/mtcnn/onet.npy',
-	'shape_predictor': 'shape_predictor_68_face_landmarks.dat',
-	'moco': 'pretrained_models/moco_v2_800ep_pretrain.pth.tar'
+	'stylegan_ffhq': '/app/pretrained_models/stylegan2-ffhq-config-f.pt',
+	'ir_se50': '/app/pretrained_models/model_ir_se50.pth',
+	'circular_face': '/app/pretrained_models/CurricularFace_Backbone.pth',
+	'mtcnn_pnet': '/app/pretrained_models/mtcnn/pnet.npy',
+	'mtcnn_rnet': '/app/pretrained_models/mtcnn/rnet.npy',
+	'mtcnn_onet': '/app/pretrained_models/mtcnn/onet.npy',
+	'shape_predictor': '/app/shape_predictor_68_face_landmarks.dat',
+	'moco': '/app/pretrained_models/moco_v2_800ep_pretrain.pth.tar'
 }
